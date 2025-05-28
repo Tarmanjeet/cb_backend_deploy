9481e55d2d2f328ae40699e2d40b9a09aebdf018
Tarmanjeet <tarmanjeetkaur@gmail.com>
2025-05-28 22:37:24 +0530

    Cors removed

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import wishlistRouter from "./routes/wishlist.router.js";
 import offerRoutes from "./routes/offer.router.js";
 import messageRouter from "./routes/message.route.js";
 import path from "path";
-import cors from "cors";
 import { fileURLToPath } from "url";
 import "./db/connection.js";  
 
@@ -19,14 +18,6 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
-    exposedHeaders: ['Content-Range', 'X-Content-Range']
-}));
-
-// Increase payload size limit for file uploads
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
@@ -38,7 +29,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use('/offer', offerRoutes);
 app.use('/message', messageRouter);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ 
